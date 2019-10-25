deaf50e43558e976c11de7cf95ea1be50e6b3cfb
Steve Degermark <=>
2019-10-25 12:35:18 -0700

    useRef updates state which is read in style

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
-import React, {useState, useEffect} from 'react';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import "./App.css";
 
 function App() {
   let [x, setX] = useState(10);
+  let moveableRef = useRef(null);
 
   useEffect(() => {
-    let app = document.getElementsByClassName("App")[0];
-    console.log(`app is ${app}`);
-    app.addEventListener("mousedown", (e) => {
-      console.log("inside mousedown");
-      setX(x => x + 10);
-    });
+    const moveable = moveableRef.current;
+
+    // I need to correct the mouse.x value so that the x is relative to the container
+    let containerLeft = null;
+    const mouseMoveHandler = e => {
+      setX(e.clientX - containerLeft - 10);
+      console.log(`mousemove at ${e.clientX - containerLeft}`);
+    };
+
+    const mouseDownHandler = e => {
+      if (containerLeft === null) {
+        const container = e.currentTarget.parentElement;
+        container && (containerLeft = container.getBoundingClientRect().x);
+      }
+      moveable.addEventListener("mousemove", mouseMoveHandler);
+    };
+
+    const mouseUpHandler = e => {
+      moveable.removeEventListener("mousemove", mouseMoveHandler);
+    };
+
+    moveable.addEventListener("mousedown", mouseDownHandler);
+    moveable.addEventListener("mouseup", mouseUpHandler);
+
+    return () => {
+      moveable.removeEventListener("mousedown", mouseDownHandler);
+      moveable.removeEventListener("mousemove", mouseMoveHandler);
+    };
   }, []);
 
-  
-  
   let containerStyle = {
     position: "absolute",
     width: "300px",
@@ -23,19 +44,21 @@ function App() {
   };
 
   let moveableStyle = {
+    boxSizing: "border-box",
     position: "absolute",
-    top: "10px",
+    top: "3px",
     left: `${x}px`,
-    width: "10px",
-    height: "10px",
-    backgroundColor: "#8C1"
+    padding: "5px",
+    width: "24px",
+    height: "24px",
+    backgroundColor: "#8C1",
+    backgroundClip: "content-box"
   };
-
 
   return (
     <div className="App">
       <div style={containerStyle}>
-        <div id="moveable" style={moveableStyle}></div>
+        <div id="moveable" ref={moveableRef} style={moveableStyle} />
       </div>
     </div>
   );
